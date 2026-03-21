5ea54b362a15ed774a4c7a43cf1371c1efe59122
giorgos <georgios.balatzis@gmail.com>
2026-03-21 14:05:20 +0200

    fix: correct base path for /ghostcar/

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // For username.github.io root repo use '/'
-  // For project repos (github.com/user/repo) use '/repo-name/'
-  base: '/',
+  base: '/ghostcar/',
 })
